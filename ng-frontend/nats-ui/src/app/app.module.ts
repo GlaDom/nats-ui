3ba7a278a9a66513489105363a413fc436ab3e5e
GlaDom <dominik8.glaser@web.de>
2022-12-15 17:33:45 +0100

    add 'add' icon to server button

--- ng-frontend/nats-ui/src/app/app.module.ts
+++ ng-frontend/nats-ui/src/app/app.module.ts
@@ -6,11 +6,12 @@ import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { ServerinformationComponent } from './serverinformation/serverinformation.component';
 import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon'
+import {MatListModule} from '@angular/material/list';
+import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatTableModule} from '@angular/material/table';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatListModule} from '@angular/material/list';
-import {MatDividerModule} from '@angular/material/divider';
 
 
 @NgModule({
@@ -24,6 +25,7 @@ import {MatDividerModule} from '@angular/material/divider';
     AppRoutingModule,
     MatButtonModule,
     MatDividerModule,
+    MatIconModule,
     MatListModule,
     MatSidenavModule,
     MatTableModule,
